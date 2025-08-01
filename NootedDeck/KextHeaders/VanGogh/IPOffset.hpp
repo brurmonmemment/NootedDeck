5ee7527d6dc5bec6d832b121f2a1422118670cd3
brurmonemt <brurmonemt@protonmail.com>
2025-07-31 22:20:52 -0500

    Soon™

---
+++ NootedDeck/KextHeaders/VanGogh/IPOffset.hpp
@@ -0,0 +1,11 @@
+// LABEL
+
+#pragma once
+#include <IOKit/IOTypes.h>
+
+constexpr UInt32 NBIO_BASE_2 = 0xD20;
+constexpr UInt32 GC_BASE_0 = 0x2000;
+constexpr UInt32 SDMA0_BASE_0 = 0x1260;
+constexpr UInt32 MP0_BASE_0 = 0x16000;
+constexpr UInt32 SMUIO_BASE_0 = 0x16800;
+constexpr UInt32 MP1_PUBLIC = 0x3B00000;
